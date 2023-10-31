07cee9f7274eeb5a5be43cddd31ec03750a5bf50
RodrigoAN97 <rodrigo_neves97@outlook.com>
2023-10-31 20:13:58 -0300

    feat: login

--- cruzeiro.py
+++ cruzeiro.py
@@ -1,6 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
-driver=webdriver.Chrome(options=options)
+driver = webdriver.Chrome(options=options)
 driver.get("https://socio5estrelas.com.br/home")
+
+cpf = driver.find_element(By.ID, 'mat-input-0')
+cpf.send_keys(os.getenv('CPF'))
+
+pw = driver.find_element(By.ID, 'mat-input-1')
+pw.send_keys(os.getenv('PASSWORD'))
+
+driver.find_element(By.XPATH, '//button[text()=" ENTRAR "]').click()
+
+time.sleep(2)
+driver.find_element(By.XPATH, '//a[text()=" EXPERIÊNCIAS "]').click()
