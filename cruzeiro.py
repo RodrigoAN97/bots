d1f1c207813cd3496b7250b9840020a946422548
RodrigoAN97 <rodrigo_neves97@outlook.com>
2023-11-01 19:56:13 -0300

    alarm

--- cruzeiro.py
+++ cruzeiro.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 import time
 import os
 from dotenv import load_dotenv
+from pygame import mixer 
 
 load_dotenv()
 options = webdriver.ChromeOptions()
@@ -20,3 +21,16 @@ driver.find_element(By.XPATH, '//button[text()=" ENTRAR "]').click()
 
 time.sleep(2)
 driver.find_element(By.XPATH, '//a[text()=" EXPERIÊNCIAS "]').click()
+
+def run_alarm():
+    mixer.init()
+    mixer.music.load('loudalarm.mp3')
+    mixer.music.set_volume(1)
+    mixer.music.play()
+    time.sleep(3)
+
+run_alarm()
+run_alarm()
+run_alarm()
+
+driver.quit()
